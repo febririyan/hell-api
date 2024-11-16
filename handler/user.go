b50e3cc301ca3dab5bf3c172ab978172c7c85249
Febri Riyanto <febririyanto@192.168.1.26>
2024-11-16 11:36:53 +0700

    uploud api register

---
+++ handler/user.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"bwastartup/user"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userSevice user.Service) *userHandler {
+	return &userHandler{userSevice}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// map input dari user ke struct RegisterUserInput
+	// struct di atas kita passing sebagai parameter service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	c.JSON(http.StatusOK, user)
+}
